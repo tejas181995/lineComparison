06304a00cda79faa8982e4fddc78149ea98fb38b
tejas181995 <tejasdev92@gmail.com>
2021-02-21 11:49:26 +0530

    file updated with Line comparison

--- LineComparison.java
+++ LineComparison.java
@@ -1,6 +1,48 @@
 
+import java.util.Scanner;
+
+class Line{
+    float x1, y1, x2, y2;
+    public Line(float x1, float y1, float x2, float y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public double length(){
+        return Math.sqrt((x2 - x1) * (x2 -x1) + (y2 - y1) *  (y2 - y1));
+    }
+    
+    public boolean equals(Line l2){
+        return this.length() == l2.length();
+    }
+
+}
+
 class LineComparison {
-    public static void main(String []args) {
-        System.out.println("Welcome to line Comparison problem");
-	}
+    public static void main(String[] args) {
+        float x1, y1, x2, y2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st point of line 1: ");
+        x1 = sc.nextFloat();
+        y1 = sc.nextFloat();
+        System.out.println("enter 2nd point of line 1: ");
+        x2 = sc.nextFloat();
+        y2 = sc.nextFloat();
+        Line l1 = new Line(x1, y1, x2, y2);
+
+        System.out.println("enter 1st point of line 2: ");
+        x1 = sc.nextFloat();
+        y1 = sc.nextFloat();
+        System.out.println("enter 2nd point of line 2: ");
+        x2 = sc.nextFloat();
+        y2 = sc.nextFloat();
+        Line l2 = new Line(x1, y1, x2, y2);
+
+        if(l1.equals(l2)){
+            System.out.println("both lines are equal");
+        }else{
+            System.out.println("both lines are not equal");
+        }
+    }
 }
